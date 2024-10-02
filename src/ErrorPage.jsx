fc4d9fa82aed98525d064a4908d34e6e1fb0cc59
jellybro99 <blg6958@utulsa.edu>
2024-10-02 13:16:40 -0500

    Initial Commit

---
+++ src/ErrorPage.jsx
@@ -0,0 +1,27 @@
+import { useRouteError } from "react-router-dom";
+import styled from "styled-components";
+import img from "./assets/boowomp.png";
+
+function ErrorPage() {
+    const error = useRouteError();
+    console.error(error);
+
+    const ErrorDiv = styled.div`
+        display:flex;
+        justify-content:center;
+        width:100%;
+        height:100%;
+    `
+
+    const ImageStyling = styled.img`
+        width:500px;
+    `
+    
+    return (
+        <ErrorDiv>
+            {error.status == 404? <ImageStyling src={img}/> : <i>{error.statusText || error.message}</i>}
+        </ErrorDiv>
+    )
+}
+
+export default ErrorPage;
---
+++ src/main.jsx
@@ -0,0 +1,47 @@
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { createGlobalStyle } from 'styled-components';
+import ErrorPage from './ErrorPage';
+
+const GlobalStyle = createGlobalStyle`
+  *, *::before, *::after {
+  box-sizing: border-box;
+  }
+  * {
+    margin: 0;
+    font-family: "Comic Sans MS", "Comic Sans", cursive;
+  }
+  body {
+    line-height: 1.5;
+    -webkit-font-smoothing: antialiased;
+  }
+  img, picture, video, canvas, svg {
+    display: block;
+    max-width: 100%;
+  }
+  input, button, textarea, select {
+    font: inherit;
+  }
+  p, h1, h2, h3, h4, h5, h6 {
+    overflow-wrap: break-word;
+  }
+  #root, #__next {
+    isolation: isolate;
+  }
+`
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <div>hello world.</div>,
+    errorElement: <ErrorPage/>,
+  }
+])
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <GlobalStyle/>
+    <RouterProvider router={router} />
+  </StrictMode>,
+)
