ef15e577163008c1ceda657d767bcb820dc3206d
jellybro99 <blg6958@utulsa.edu>
2024-10-18 13:32:14 -0500

    Product page styling

--- src/components/ProductPage.jsx
+++ src/components/ProductPage.jsx
@@ -1,5 +1,36 @@
 import { useOutletContext, useParams } from "react-router-dom";
 import Image from "./Image";
+import styled from "styled-components";
+
+const ProductPageDiv = styled.div`
+    padding-left: 3rem;
+    padding-right: 3rem;
+    padding-top: 2rem;
+    padding-bottom: 2rem;
+    display: flex;
+    height: 50rem;
+    gap: 2rem;
+`;
+
+const SideArea = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 2rem;
+`;
+
+const AddToCart = styled.button`
+    align-self: center;
+    width: 100%;
+    cursor: pointer;
+    background: none;
+    &:hover {
+        background-color: #8ace00;
+    }
+`;
+
+const StyledImage = styled(Image)`
+    border: solid black 1px;
+`;
 
 function ProductPage() {
     const brands = useOutletContext()[0];
@@ -11,13 +42,16 @@ function ProductPage() {
     if (product === undefined) throw new Response("Not Found", { status: 404 });
 
     return (
-        <div>
-            <h1>{product.name}</h1>
-            <Image src={product.image} />
-            <button onClick={() => handleCartAdd(brandId, productId)}>
-                add to cart
-            </button>
-        </div>
+        <ProductPageDiv>
+            <StyledImage src={product.image} />
+            <SideArea>
+                <h1>{product.name}</h1>
+                <p>{product.about}</p>
+                <AddToCart onClick={() => handleCartAdd(brandId, productId)}>
+                    add to cart
+                </AddToCart>
+            </SideArea>
+        </ProductPageDiv>
     );
 }
 
