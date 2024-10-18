4d7854657ae1c16203b237d269b98ff3c5fe5b2a
jellybro99 <blg6958@utulsa.edu>
2024-10-18 08:20:38 -0500

    Image functionality

--- src/components/Image.jsx
+++ src/components/Image.jsx
@@ -1,12 +1,28 @@
 import styled from "styled-components";
+import PropTypes from "prop-types";
 
 const StyledImage = styled.img`
+    //need to figure out how to use props with styled css
     max-height: 100%;
+    cursor: ${(props) => props.$cursor || "inherit"};
 `;
 
 function Image(props) {
+    const { src, cursor, ...ImageProps } = props;
     //can add alt image for load
-    return <StyledImage {...props} loading="lazy" />;
+    return (
+        <StyledImage
+            $cursor={cursor}
+            src={src == "" ? "\\src\\assets\\image-not-found.jpg" : src}
+            {...ImageProps}
+            loading="lazy"
+        />
+    );
 }
+
+Image.propTypes = {
+    src: PropTypes.string,
+    cursor: PropTypes.string,
+};
 
 export default Image;
